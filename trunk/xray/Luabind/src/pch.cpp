c86da974511480f4247fe69f574a68c4856c75aa
KRodinn <stalker.moderator@gmail.com>
2017-02-25 21:52:51 +0300

    Luabind without boost
    Отсюда: https://github.com/Im-dex/xray-162
    Этот вариант подходит больше, чем Luabind 0.9+. Намного меньше проблем с
    адаптацией.

---
+++ trunk/xray/Luabind/src/pch.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+/*#ifdef NDEBUG
+
+namespace std {
+	void terminate			()
+	{
+		abort				();
+	}
+} // namespace std
+
+#endif // #ifdef NDEBUG*/
---
+++ trunk/xray/Luabind/src/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <luabind/lua_include.hpp>
+#include <luabind/luabind.hpp>
+
+/*#ifdef NDEBUG
+
+namespace std {
+	void terminate();
+} // namespace std
+
+#endif // #ifdef NDEBUG*/
