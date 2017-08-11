7e24badfd207a456aebe8c6596a234dbbfc03ac3
KRodin <stalker.moderator@gmail.com>
2017-08-11 18:08:24 +0300

    [НС2016] Personal_test из OGSE

---
+++ trunk/gamedata_NS2016/scripts/personal_test.lua
@@ -0,0 +1,10 @@
+--[[---------------------------------------------------------
+local ebt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+local rbt = "ÀÁÂÃÄÅ¨ÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞ‗"
+local emt = "abcdefghijklmnopqrstuvwxyz"
+local rmt = "אבגדהו¸זחטיךכלםמןנסעףפץצקרשתûü‎‏ÿ"
+log1(ebt:lower())
+log1(rbt:lower())
+log1(emt:upper())
+log1(rmt:upper())
+--]]---------------------------------------------------------
